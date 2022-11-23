4540730d9dbbd0c33ed118b7dc7ade35a9919630
Jibran Abdul <notbenderz@gmail.com>
2022-11-23 22:01:51 +0000

    First edit 	modified:   package-lock.json 	modified:   package.json 	deleted:    public/favicon.ico 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/Dashboard.js 	new file:   src/Login.js 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   src/useAuth.js

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+
+
+const AUTH_URL = 
+"https://accounts.spotify.com/authorize?client_id=b1de9c42b86d45b39aa1193168b2a4af&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+export default function Login() {
+  return (
+    <Container>
+        <a className="btn btn-success btn-lg" href={AUTH_URL}>
+            lOGIN WITH SPOTIFY
+        </a>
+    </Container>
+  )
+}
